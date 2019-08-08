e89e8200bc6116554f24f086ab6da765578ca610
Pete Johnson <petecj2@gmail.com>
2019-08-07 21:18:13 -0400

    Skeleton for web root processing

---
+++ core/batch/webRoot.py
@@ -0,0 +1,10 @@
+import os
+import boto3
+
+def webRoot(event, context):
+    # Echo inputs
+    print('TEMPLATES_BUCKET_NAME: ' + os.environ['TEMPLATES_BUCKET_NAME'])
+    print('WEB_BUCKET_NAME: ' + os.environ['WEB_BUCKET_NAME'])
+    print('PACKAGES_DYNAMODB_TABLE: ' + os.environ['PACKAGES_DYNAMODB_TABLE'])
+
+    return
