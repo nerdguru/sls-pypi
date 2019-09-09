4905486967d54401cd0d310c42a12726145d803c
Pete Johnson <petecj2@gmail.com>
2019-09-09 14:35:45 -0400

    Added missing message passing to refresh home page when an add/update happens

--- core/batch/dynamoPackage.py
+++ core/batch/dynamoPackage.py
@@ -5,6 +5,7 @@ def dynamoPackage(event, context):
     # Echo inputs
     print('PACKAGES_DYNAMODB_TABLE: ' + os.environ['PACKAGES_DYNAMODB_TABLE'])
     print('WEB_TOPIC_ARN: ' + os.environ['WEB_TOPIC_ARN'])
+    print('WEB_ROOT_TOPIC_ARN: ' + os.environ['WEB_ROOT_TOPIC_ARN'])
     print('REMOVE_WEB_TOPIC_ARN: ' + os.environ['REMOVE_WEB_TOPIC_ARN'])
     print('Event: ' + event['Records'][0]['eventName'])
 
@@ -18,12 +19,19 @@ def dynamoPackage(event, context):
         )
         print(response)
     else:
+        # Add message to SNS topic that'll force a refresh of the package page page
         print('Add or modify record:')
         print(event['Records'][0]['dynamodb']['NewImage']['package']['S'])
         sns = boto3.client('sns')
         response = sns.publish(
             TopicArn=os.environ['WEB_TOPIC_ARN'],
             Message=event['Records'][0]['dynamodb']['NewImage']['package']['S'],
+        )
+        print(response)
+        # Add message to SNS topic that'll force a refresh of the home page
+        response = sns.publish(
+            TopicArn=os.environ['WEB_ROOT_TOPIC_ARN'],
+            Message='Refresh, ' + event['Records'][0]['dynamodb']['NewImage']['package']['S'] + 'added/updated',
         )
         print(response)
 
